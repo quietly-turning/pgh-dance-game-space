463775865b5b25a681f046e60415aeba8b518b36
quietly-turning <quietly.turning@gmail.com>
2025-07-30 18:42:22 -0400

    adjust responsive layout of header

--- src/_layout/Header.jsx
+++ src/_layout/Header.jsx
@@ -57,7 +57,7 @@ class Header extends Component {
 					<span className="navbar-toggler-icon"></span>
 				</button>
 
-				<div className="navbar-nav w-25">
+				<div className="navbar-nav">
 					<NavLink style={{display:"inline-flex"}} className="ms-4 me-4 nav-link text-nowrap text-white font-weight-normal" exact="true" to="/">
 						<span className="d-none d-sm-block">PGH Dance Game Space</span>
 					</NavLink>
@@ -76,30 +76,29 @@ class Header extends Component {
 					</div>
 				</div>
 
-					<div className="collapse navbar-collapse w-50" id="navbarNav">
-						<ul className="navbar-nav" style={{flexGrow:"1", justifyContent:"center"}}>
-							<li className="nav-item">
-								<NavLink className="nav-link" exact="true" to="/">About</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink className="nav-link" to="/Events">Events</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink className="nav-link" to="/Contact">Contact</NavLink>
-							</li>
-						</ul>
-					</div>
+				<div className="collapse navbar-collapse w-50" id="navbarNav">
+					<ul className="navbar-nav" style={{flexGrow:"1", justifyContent:"center"}}>
+						<li className="nav-item">
+							<NavLink className="nav-link" exact="true" to="/">About</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/Events">Events</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/Contact">Contact</NavLink>
+						</li>
+					</ul>
+				</div>
 
-					<div className="colorMode-toggle form-check form-switch w-25 me-3 justify-content-end d-none d-sm-flex">
-						<label className="form-check-label" htmlFor="lightSwitchFullWidth">
-							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-brightness-high" viewBox="0 0 16 16">
-								<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"></path>
-								<path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"></path>
-							</svg>
-							<input className="form-check-input lightswitch" style={{position:"relative", top:"2px", right:"3px"}} type="checkbox" id="lightSwitchFullWidth" onChange={this.toggleColorMode} />
-						</label>
-
-					</div>
+				<div className="colorMode-toggle form-check form-switch w-25 me-3 justify-content-end d-none d-sm-flex">
+					<label className="form-check-label" htmlFor="lightSwitchFullWidth">
+						<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-brightness-high" viewBox="0 0 16 16">
+							<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"></path>
+							<path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"></path>
+						</svg>
+						<input className="form-check-input lightswitch" style={{position:"relative", top:"2px", right:"3px"}} type="checkbox" id="lightSwitchFullWidth" onChange={this.toggleColorMode} />
+					</label>
+				</div>
 			</nav>
 		);
 	}
